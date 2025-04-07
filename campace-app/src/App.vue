ce011295b25a480b85faba5dd2b15dea688128d6
Niloufar Karami <niluabi00@gmail.com>
2025-04-07 21:14:34 +0200

    initial projects

---
+++ campace-app/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    Hello world!!  
+  </div>
+</template>
+
+<script>
+
+  // Export
+  export default {
+    name: 'App',
+    data() {
+      return {
+        activePage: ""
+      }
+    },
+    components: {
+
+    },
+    methods: {
+      setActivePage(page) {
+        this.activePage = page;
+      }
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
